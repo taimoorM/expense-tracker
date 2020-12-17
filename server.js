6639871fc5140408fa3c5b50926bbf626bbcf416
Taimoor M <taimoor.m89@gmail.com>
2020-12-16 21:12:56 -0500

    setup server

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const { port, mongoURI } = require("./config");
+const transactionsRoutes = require("./routes/transactions");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
 
 mongoose
   .connect(mongoURI, {
@@ -11,6 +19,8 @@ mongoose
   })
   .then(() => console.log("MongoDB is connected"))
   .catch((err) => console.log(err));
+
+app.use("/api/transactions", transactionsRoutes);
 
 app.get("/", (req, res) => res.send("hello world"));
 
